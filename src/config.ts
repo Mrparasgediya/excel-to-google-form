f2e9ba697a27f38db6a9da2b8d95a6602db2cb1b
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-10 07:21:54 +0530

    modified config for dynamic url for fetch

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,10 @@
-const config = (() => {
-    const currentBaseFetchUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://excel-to-google-form.vercel.app';
-    return {
-        JWT_SECRET: process.env.JWT_SECRET!,
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
-        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
-        FETCH_BASE_URL: currentBaseFetchUrl,
-        GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI && `${currentBaseFetchUrl}${process.env.GOOGLE_REDIRECT_URI}` || undefined,
-        GOOGLE_SCOPE: process.env.GOOGLE_SCOPE && JSON.parse(process.env.GOOGLE_SCOPE!) || undefined,
-    }
-})();
+const config = {
+    JWT_SECRET: process.env.JWT_SECRET!,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
+    FETCH_BASE_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://excel-to-google-form.vercel.app',
+    GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI!,
+    GOOGLE_SCOPE: process.env.GOOGLE_SCOPE && JSON.parse(process.env.GOOGLE_SCOPE!) || undefined,
+}
 
 export default config;
