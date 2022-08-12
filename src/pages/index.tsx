c119b11eb12a4e4d7308baf0065c05a5ac0a5cec
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-12 18:20:39 +0530

    Updated home page and added github page documentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,54 +2,26 @@ import Button from "components/Button";
 import ButtonLink from "components/ButtonLink";
 import withLayout from "components/withLayout";
 import type { NextPage } from "next";
+import NextLink from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div className="space-y-4 glass glass--white p-4">
       <h2 className="font-bold text-3xl">Excel To Google Form Converter</h2>
       <h3 className="font-smibold text-xl">
-        Notes To Remember for preparing excel file
+        Watch Documentation To Prepare Excel File
       </h3>
-      <ul className="space-y-2 font-normal text-md">
-        <li>
-          Row 1: must contain heading only which will be field name for form
-        </li>
-        <li>
-          <span>Row 2: should be empty or has following details</span>
-          <ul className="list-disc ml-16 space-y-2">
-            <li>
-              For Radio Button:{" "}
-              <em className="font-medium">
-                t=radio;v=(radio option seperated by comma(,));
-              </em>
-              (Ex: t=radio;v=male,female;)
-            </li>
-            <li>
-              For Drop Down:{" "}
-              <em className="font-medium">
-                t=dropdown;v=(drop down option seperated by comma(,));
-              </em>{" "}
-              (Ex: t=dropdown;v=Ahmedabad,Surat,Baroda;)
-            </li>
-            <li>
-              For Check box:{" "}
-              <em className="font-medium">
-                t=checkbox;v=(checkbox option seperated by comma(,));
-              </em>{" "}
-              (Ex: t=checkbox;v=Reading,Gaming,Walking,Singing;)
-            </li>
-          </ul>
-        </li>
-        <li>
-          From Row 3: your data values{" "}
-          <em className="font-medium">
-            (Note: you must need to enter one record)
-          </em>
-        </li>
-      </ul>
-      <ButtonLink href="/convert">
-        <Button classes="mt-6">Convert Excel To Form</Button>
-      </ButtonLink>
+      <div className="flex items-center gap-2 border-2">
+        <ButtonLink
+          target="_blank"
+          href="https://github.com/Mrparasgediya/excel-to-google-form"
+        >
+          <Button>Watch Documentation</Button>
+        </ButtonLink>
+        <ButtonLink href="/convert">
+          <Button>Convert Excel To Form</Button>
+        </ButtonLink>
+      </div>
     </div>
   );
 };
